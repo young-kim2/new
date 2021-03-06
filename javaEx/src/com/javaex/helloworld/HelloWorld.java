e1807de74559dd2a116a347bb0a00963087d7bc9
김영 <@LAPTOP-T579MPBH>
2021-03-06 11:20:12 +0900

    연습1

---
+++ javaEx/src/com/javaex/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.javaex.helloworld;
+
+/*
+ * 목표: Hello World! 문자열을 출력한다
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		//Hello World 문자열 출력
+		System.out.println("Hello World");
+	}
+
+}
