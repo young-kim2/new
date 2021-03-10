519a60baae4865a922bb2b19c1471075d6aff93c
김영 <dhehd215@daum.net>
2021-03-10 19:19:33 +0900

    Practice01-1

---
+++ javaEx/src/com/javaex/practice01/Problem01.java
@@ -0,0 +1,14 @@
+package com.javaex.practice01;
+
+public class Problem01 {
+
+	public static void main(String[] args) {
+		//1~100 수 중에서 5의 배수,7의 배수만 출력
+		for(int num=1;num<=100;num++) {
+			if(num%5==0 && num%7==0) {
+				System.out.println(num);
+			}
+		}
+	}
+
+}
---
+++ javaEx/src/com/javaex/practice01/problem03.java
@@ -0,0 +1,14 @@
+package com.javaex.practice01;
+
+public class problem03 {
+
+	public static void main(String[] args) {
+	for(int num=1; num<=9; num++) {
+		for(int dan=2;  dan<=9; dan++) {
+			System.out.printf("%d*%d=%d\t",dan,num,dan*num);
+		}
+		System.out.println();
+	}
+	}
+
+}
